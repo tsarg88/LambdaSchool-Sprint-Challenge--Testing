e7fd493b8920099891f4f6600cfdb12f367d5678
umbur <you@example.com>
2019-04-12 10:39:19 -0600

    Created games array of object; wrote test cases for upcoming  GET endpoint

--- api/server.js
+++ api/server.js
@@ -7,6 +7,25 @@ server.get('/', (req, res) => {
     res.status(200).json({ message: 'Server is working' });
 })
 
+// An array of object to beused to mock database
+const games = [
+    {
+        title: 'Pacman',
+        genre: 'Arcade',
+        releaseYear: 1980
+    },
+    {
+        title: 'Final Fantasy',
+        genre: 'RPG',
+        releaseYear: 1997
+    },
+    {
+        title: 'Parasite Eve',
+        genre: 'Horror',
+        releaseYear: 1995
+    }
+]
+
 
 
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -3,7 +3,7 @@ const server = require('./server.js');
 
 describe('server.js', () => {
     describe('GET /', () => {
-        it('should respond with 200 OK async', async () => {
+        it('Should respond with 200 OK async', async () => {
             const response = await request(server).get('/');
       
             expect(response.status).toBe(200);
@@ -16,5 +16,42 @@ describe('server.js', () => {
             expect(res.body).toEqual({ message: 'Server is working' });
           });
       });
+      // *** GET /games test here: ***
+      describe('GET /games endpoint tests', () => {
+        it('Should return status code 200(OK)', async () => {
+            const response = await request(server).get('/games')
+
+            expect(response.status).toBe(200)
+        })
+        it('Should return JSON', async () => {
+            const response = await request(server).get('/games')
+
+            expect(response.type).toBe('application/json')
+        })
+        it('Should return correct array of game objects', async () => {
+            const response = await request(server).get('/games')
+
+            const expected = [
+                {
+                    title: 'Pacman',
+                    genre: 'Arcade',
+                    releaseYear: 1980
+                },
+                {
+                    title: 'Final Fantasy',
+                    genre: 'RPG',
+                    releaseYear: 1997
+                },
+                {
+                    title: 'Parasite Eve',
+                    genre: 'Horror',
+                    releaseYear: 1995
+                }
+            ]
+
+            expect(response.body).toEqual(expected);
+            expect(Array.isArray(response.body)).toBe(true)
+        })
+    })
 })
         
